45f24e390705153254a892cd1c82a9b542616429
Alex <alexxcamargo1000@gmail.com>
2023-05-22 11:46:02 -0300

    feat: aula 4

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -3,10 +3,9 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
+  const code = searchParams.get('code')
 
   const redirectTo = request.cookies.get('redirectTo')?.value
-
-  const code = searchParams.get('code')
 
   const registerResponse = await api.post('/register', {
     code,
@@ -20,7 +19,7 @@ export async function GET(request: NextRequest) {
 
   return NextResponse.redirect(redirectURL, {
     headers: {
-      'Set-cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds};`,
+      'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds};`,
     },
   })
 }
